59b9903c050d132727d52c3c0968b2a9536ce3b7
idilsaglam <idilsaglam1@hotmail.com>
2021-12-29 18:31:21 +0100

    feat(client): BoardPane updated
    Size added in constructor parameter
    Pane initialization moved in constructor from the initPanes function
    Panes sized with game pane size

--- client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
@@ -9,23 +9,36 @@ import org.catanuniverse.commons.ClientConfiguration;
 public class BoardPane extends JPanel {
 
   private final ClientConfiguration configuration;
-  private JPanel topStatusPane, gameBoardPane, bottomStatusPane;
+  private final JPanel topStatusPane, gameBoardPane, bottomStatusPane;
 
   public BoardPane(Dimension size, ClientConfiguration configuration) {
     this.initSizes(size);
     // TODO: Check if we need to update the layout
     this.configuration = configuration;
-    this.initPanes();
+    this.topStatusPane = new TopStatusBar();
+    this.gameBoardPane = new GameBoardPane();
+    this.bottomStatusPane = new BottomStatusBar();
+    this.initPanes(size);
   }
 
 
   /**
-   * Initialise panes
+   * Initialise panes and add them to the current JPanel
    */
-  private void initPanes() {
-    this.topStatusPane = new TopStatusBar();
-    this.gameBoardPane = new GameBoardPane();
-    this.bottomStatusPane = new BottomStatusBar();
+  private void initPanes(Dimension size) {
+    final Dimension sideSize = new Dimension(
+        size.width,
+        size.height / 8
+    ), centerSize = new Dimension(
+        size.width,
+        3 * size.height / 4
+    );
+    this.topStatusPane.setSize(sideSize);
+    this.topStatusPane.setPreferredSize(sideSize);
+    this.gameBoardPane.setSize(centerSize);
+    this.gameBoardPane.setPreferredSize(centerSize);
+    this.bottomStatusPane.setSize(sideSize);
+    this.bottomStatusPane.setPreferredSize(sideSize);
     super.add(this.topStatusPane, BorderLayout.PAGE_START);
     super.add(this.gameBoardPane, BorderLayout.CENTER);
     super.add(this.bottomStatusPane, BorderLayout.PAGE_END);
