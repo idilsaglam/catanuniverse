cc5cccf1458c95f68abb26fc30aa5445e471009f
idilsaglam <idilsaglam1@hotmail.com>
2021-12-31 02:19:57 +0100

    style(core): player class indentened with spotless

--- core/src/main/java/org/catanuniverse/core/game/Player.java
+++ core/src/main/java/org/catanuniverse/core/game/Player.java
@@ -6,17 +6,36 @@
 package org.catanuniverse.core.game;
 
 public class Player {
-  private String username;
+    private String username;
 
-  public Player(String username) {
-      this.username = username;
-  }
+    /**
+     * Create a player with the given username
+     * @param username The username of the player to create
+     */
+    public Player(String username) {
+        this.username = username;
+    }
 
-  public Player() {
-    this("");
-  }
+    /**
+     * Create a player instance with an empty username
+     */
+    public Player() {
+        this("");
+    }
 
-  public String getUsername() {
-    return this.username;
-  }
+    /**
+     * Get the username of the current player
+     * @return The username of the current player
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * Verify if the given player is valid or not
+     * @return True if the given player is valid, false if not
+     */
+    public boolean isValid() {
+        return this.username.matches("^[a-zA-Z0-9]{4,10}$");
+    }
 }
