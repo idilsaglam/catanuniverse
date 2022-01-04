4a745c67a70a66cc344a1d03c475f9f627871034
idilsaglam <idilsaglam1@hotmail.com>
2022-01-04 01:35:54 +0100

    feat(core): GroundType updated with new functions
    Getting color related to the ground type and generating a random ground type methods implemented

--- core/src/main/java/org/catanuniverse/core/game/GroundType.java
+++ core/src/main/java/org/catanuniverse/core/game/GroundType.java
@@ -5,6 +5,8 @@
 */
 package org.catanuniverse.core.game;
 
+import java.util.Random;
+
 public enum GroundType {
     Forest(Resource.Wood),
     Hill(Resource.Clay),
@@ -28,5 +30,30 @@ public enum GroundType {
      */
     protected Resource produces() {
         return this.resource;
+    }
+
+    public int getColor() {
+        return switch (this) {
+            case Water -> 0x5D9BC2;
+            case Forest -> 0xFEC437;
+            case Hill -> 0xB3C92F;
+            case Meadow -> 0x19DCCD;
+            case Farm -> 0xCD7730;
+            case Mountain -> 0xF2D2A9;
+            case Desert -> 0x4B7922;
+        };
+    }
+    // Black color code 0x000000
+    public static GroundType random(boolean desertExists) {
+        Random r = new Random();
+        return switch (r.nextInt(7)) {
+            case 0 -> GroundType.Forest;
+            case 1 -> GroundType.Hill;
+            case 2 -> GroundType.Meadow;
+            case 3 -> GroundType.Farm;
+            case 4 -> GroundType.Mountain;
+            // TODO: There should be only one desert
+            default -> desertExists ? random(true) : GroundType.Desert;
+        };
     }
 }
