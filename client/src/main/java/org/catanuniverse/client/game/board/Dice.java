bc7dbc6e39a7e28c58923e847103b3b20696d74d
idilsaglam <idilsaglam1@hotmail.com>
2022-01-05 18:54:48 +0100

    Dice updated

--- client/src/main/java/org/catanuniverse/client/game/board/Dice.java
+++ client/src/main/java/org/catanuniverse/client/game/board/Dice.java
@@ -11,37 +11,54 @@ class Dice extends JPanel {
     private static final long serialVersionUID = 1L;
     static Random rand = new Random();  //  @jve:decl-index=0:
     private int die1 = 1;
+    private int die2 = 1;
     private JButton rollBtn = null;
     private JTextField dieValueTF = null;
+    private JTextField dieValueTF2 = null;
     private JLabel dieLabel = null;
+    private JLabel dieLabel2 = null;
     private int top = 50, left = 100;
 
     private BufferedImage dieImage;
+    private BufferedImage dieImage2;
     private JLabel dieImageLabel;
+    private  JLabel dieImageLabel2;
     public Dice() {
 
-
-
         dieImage = new BufferedImage(100,100, BufferedImage.TYPE_INT_RGB);
+        dieImage2 = new BufferedImage(100,100,BufferedImage.TYPE_INT_RGB);
+
         this.setSize(6370, 3160);
-        this.setPreferredSize(new Dimension(400, 200));
+        this.setPreferredSize(new Dimension(900, 450));
         this.setBackground(Color.pink);
 
+
         dieLabel = new JLabel();
-        dieLabel.setBounds(new Rectangle(147, 28, 60, 20));
+        dieLabel.setBounds(new Rectangle(147, 28, 67, 34));
         dieLabel.setText("Die Value:");
+
+        dieLabel2 = new JLabel();
+        dieLabel2.setBounds(new Rectangle(300, 28, 67, 34));
+        dieLabel2.setText("Die Value 2:");
 
         this.setLayout(null);
 
         this.add(getDieValueTF());
-        this.add(dieLabel, null);
+        this.add(getDieValueTF2());
+        this.add(dieLabel);
+        this.add(dieLabel2);
+
         dieImageLabel = new JLabel(new ImageIcon(dieImage));
         dieImageLabel.setBounds(200,100, 100,100);
         this.add(dieImageLabel);
-        this.setBorder(new EmptyBorder(1000, 2000, 1000, 400));
+
+        dieImageLabel2 = new JLabel(new ImageIcon(dieImage2));
+        dieImageLabel2.setBounds(100,100,100,100);
+        this.add(dieImageLabel2);
 
         this.add(getRollBtn());
         displayDie();
+        displayDie2();
     }
 
 
@@ -57,6 +74,7 @@ class Dice extends JPanel {
             rollBtn.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
                     displayDie();
+                    displayDie2();
                     roll();
                 }
             });
@@ -72,6 +90,16 @@ class Dice extends JPanel {
             //dieValueTF.setHorizontalAlignment(JTextField.CENTER);
         }
         return dieValueTF;
+    }
+
+    private JTextField getDieValueTF2() {
+
+        if (dieValueTF2 == null) {
+            dieValueTF2 = new JTextField();
+            dieValueTF2.setBounds(new Rectangle(500, 32, 26, 26));
+            dieValueTF.setHorizontalAlignment(JTextField.CENTER);
+        }
+        return dieValueTF2;
     }
 
     void displayDie(){
@@ -118,8 +146,53 @@ class Dice extends JPanel {
 
     }
 
+    void displayDie2(){
+        dieValueTF2.setText(String.valueOf(die2));
+        Graphics g = dieImage2.getGraphics();
+        try {
+            g.setColor(Color.WHITE);
+            g.fillRect(0,0, 100,100);
+            g.setColor(Color.BLACK);
+            g.drawRect(0,0, 100,100);
+            switch (die2) {
+                case 1:
+                    g.fillOval(50,50, 10,10);
+                    break;
+                case 3:
+                    g.fillOval(20,20, 10,10);
+                    g.fillOval(50,50, 10,10);
+                    g.fillOval(80,80, 10,10);
+                    break;
+                case 5:
+                    g.fillOval(50,50, 10,10);
+                    // Fall through.
+                case 4:
+                    g.fillOval(80,20, 10,10);
+                    g.fillOval(20,80, 10,10);
+                    // Fall through.
+                case 2:
+                    g.fillOval(20,20, 10,10);
+                    g.fillOval(80,80, 10,10);
+                    break;
+                case 6:
+                    g.fillOval(20,20, 10,10);
+                    g.fillOval(20,50, 10,10);
+                    g.fillOval(80,20, 10,10);
+                    g.fillOval(20,80, 10,10);
+                    g.fillOval(80,50, 10,10);
+                    g.fillOval(80,80, 10,10);
+                    break;
+            }
+            dieImageLabel2.repaint();
+        } finally {
+            g.dispose();
+        }
+
+    }
+
     void roll() {
         die1 = rand.nextInt(6) + 1;
+        die2 = rand.nextInt(6) + 1;
     }
 
 
