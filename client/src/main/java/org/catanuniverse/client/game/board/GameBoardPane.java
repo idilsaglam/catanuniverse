d6da29bfa64cf94917e3ecc23a4d87eca7254bf8
idilsaglam <idilsaglam1@hotmail.com>
2022-01-05 16:36:54 +0100

    feat(client): game board centered

--- client/src/main/java/org/catanuniverse/client/game/board/GameBoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/GameBoardPane.java
@@ -57,7 +57,7 @@ class GameBoardPane extends JPanel {
     }
 
     private void setHexagons(int s, int radius, int padding) {
-        Point origin = new Point(size.width / 2, size.height / 2);
+        Point origin = new Point(size.width / 2, size.height / 4);
         double ang30 = Math.toRadians(30);
         double xOff = Math.cos(ang30) * (radius + padding);
         double yOff = Math.sin(ang30) * (radius + padding);
