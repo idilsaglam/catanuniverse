535f89328df8a519e78cf8cd398d34ec38328efc
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 23:24:07 +0100

    feat(core): Harbor class implemented

--- core/src/main/java/org/catanuniverse/core/game/Harbor.java
+++ core/src/main/java/org/catanuniverse/core/game/Harbor.java
@@ -5,4 +5,34 @@
 */
 package org.catanuniverse.core.game;
 
-public class Harbor {}
+import java.util.Random;
+
+public class Harbor {
+    private final Resource resource;
+    private final int coeff;
+    public Harbor(Resource resource, int coeff) {
+        this.resource = resource;
+        this.coeff = coeff;
+    }
+
+    public Harbor(int coeff) {
+        this(null, coeff);
+    }
+
+    public boolean isSpecialised() {
+        return this.resource != null;
+    }
+
+    public Resource getResource() {
+        return this.resource;
+    }
+
+    public int getCoeff() {
+        return this.coeff;
+    }
+
+    public static Harbor random() {
+        Random r = new Random();
+        return new Harbor(Resource.random(), r.nextInt(3) + 1);
+    }
+}
