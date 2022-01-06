da1f1d01c21d89c56a80c3a64b8d6ddaa8394b3f
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 01:42:07 +0100

    Board Side Pane updated

--- client/src/main/java/org/catanuniverse/client/game/board/GameBoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/GameBoardPane.java
@@ -60,7 +60,7 @@ class GameBoardPane extends JPanel {
     }
 
     private void setHexagons(int s, int radius, int padding) {
-        Point origin = new Point(size.width / 3, 3 * size.height / 8);
+        Point origin = new Point(size.width / 8, 3 * size.height / 8);
         double ang30 = Math.toRadians(30);
         double xOff = Math.cos(ang30) * (radius + padding);
         double yOff = Math.sin(ang30) * (radius + padding);
