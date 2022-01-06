6a9e1059098853f285f6d7fc02fac01f34b65e03
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 01:35:45 +0100

    Board Side Pane updated

--- client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
@@ -73,9 +73,9 @@ public class BoardPane extends JPanel {
         //this.gameBoardPane.setPreferredSize(centerSize);
         this.bottomStatusPane.setSize(sideSize);
         this.bottomStatusPane.setPreferredSize(sideSize);
-        this.gameBoardPane.setSize(new Dimension(3* centerSize.width / 4, centerSize.height));
-        this.gameBoardPane.setMinimumSize(new Dimension(3* centerSize.width / 4, centerSize.height));
-        this.gameBoardPane.setPreferredSize(new Dimension(3* centerSize.width / 4, centerSize.height));
+        this.gameBoardPane.setSize(new Dimension(1* centerSize.width / 2, centerSize.height));
+        this.gameBoardPane.setMinimumSize(new Dimension(1* centerSize.width / 2, centerSize.height));
+        this.gameBoardPane.setPreferredSize(new Dimension(1* centerSize.width / 2, centerSize.height));
 
         this.boardSidePane.setSize(new Dimension(centerSize.width /4, centerSize.height));
 
