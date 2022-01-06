70a2556ddf824d4804eaf97d0d96ed3343e93b02
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 23:06:54 +0100

    board resized

--- client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
@@ -121,7 +121,7 @@ public class BoardPane extends JPanel {
     /** Initialise panes and add them to the current JPanel */
     private void initPanes(Dimension size) {
         final Dimension topSize = new Dimension(size.width, size.height / 8),
-                bottomSize = new Dimension(size.width, 3 *size.height / 16),
+                bottomSize = new Dimension(size.width, size.height / 8),
                 centerSize = new Dimension(size.width,  11* size.height / 16);
 
         this.topStatusPane.setSize(topSize);
