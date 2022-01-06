99f2ed22c654683605f5be03ccf8393090b7c81d
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 18:21:39 +0100

    feat(core): Resource enumeration updated
    A method added to return the given image for the resource

--- core/src/main/java/org/catanuniverse/core/game/Resource.java
+++ core/src/main/java/org/catanuniverse/core/game/Resource.java
@@ -5,10 +5,29 @@
 */
 package org.catanuniverse.core.game;
 
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public enum Resource {
     Wood,
     Clay,
     Wool,
     Corn,
-    Mineral,
+    Mineral;
+
+    public Image getImage(int width, int height, int scale) throws IOException {
+        return ImageIO.read(this.getClass().getResource(String.format("/%s.png",switch (this) {
+            case Wood -> "wood";
+            case Corn -> "corn";
+            case Clay -> "argile";
+            case Mineral -> "mineral";
+            case Wool -> "sheep";
+        }))).getScaledInstance(width, height, scale);
+    }
+
+    public Image getImage() throws IOException {
+        return this.getImage(60, 60, Image.SCALE_SMOOTH);
+    }
 }
