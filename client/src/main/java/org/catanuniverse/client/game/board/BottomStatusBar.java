3779350f63caadcca337a91351d200075bd356ba
idilsaglam <idilsaglam1@hotmail.com>
2022-01-06 18:22:44 +0100

    refactor(client): BottomStatusBar refactored with an inner class

--- client/src/main/java/org/catanuniverse/client/game/board/BottomStatusBar.java
+++ client/src/main/java/org/catanuniverse/client/game/board/BottomStatusBar.java
@@ -6,113 +6,62 @@
 package org.catanuniverse.client.game.board;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Random;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.text.html.ImageView;
+import org.catanuniverse.core.game.Player;
+import org.catanuniverse.core.game.Resource;
 
 class BottomStatusBar extends JPanel {
     // label to display text
     static JLabel l;
-    int woodNumber, argileNumber,mineralNumber,bleNumber,sheepNumber;
-    public BottomStatusBar() throws IOException {
-
-        this.woodNumber= 0;
-        this.argileNumber = 0;
-        this.bleNumber = 0;
-        this.sheepNumber = 0;
-        this.mineralNumber = 0;
-
+    private Player currentPlayer;
+    private java.util.List<ResourceCard> resourceCards;
+    public BottomStatusBar(Player currentPlayer) throws IOException {
+        this.currentPlayer = currentPlayer;
+        this.resourceCards = new ArrayList<ResourceCard>();
+        // TODO: Update grid layout
         this.setLayout(new GridLayout());
-
-
         this.add(this.getResourcesRow());
-
     }
 
     public JPanel getResourcesRow() throws IOException {
         JPanel result = new JPanel();
-
-        // create a label to display text
-        l = new JLabel();
-        // add text to label
-        l.setText(""+mineralNumber);
-        // add label to panel
-        result.add(l);
-
-        JLabel mineralLabel;
-        BufferedImage mineral = ImageIO.read(this.getClass().getResource("/mineral.png"));
-        Image dimg = mineral.getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        mineralLabel = new JLabel(new ImageIcon(dimg));
-        result.add(mineralLabel);
-
-
-        // create a label to display text
-        l = new JLabel();
-        // add text to label
-        l.setText(""+sheepNumber);
-        // add label to panel
-        result.add(l);
-
-        JLabel sheepLabel;
-        BufferedImage sheep = ImageIO.read(this.getClass().getResource("/sheep.png"));
-        Image simg = sheep.getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        sheepLabel = new JLabel(new ImageIcon(simg));
-        result.add(sheepLabel);
-
-        // create a label to display text
-        l = new JLabel();
-        // add text to label
-        l.setText(""+argileNumber);
-        // add label to panel
-        result.add(l);
-
-
-        JLabel argileLabel;
-        BufferedImage argile = ImageIO.read(this.getClass().getResource("/argile.png"));
-        Image aimg = argile.getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        argileLabel = new JLabel(new ImageIcon(aimg));
-        result.add(argileLabel);
-
-
-        // create a label to display text
-        l = new JLabel();
-        // add text to label
-        l.setText(""+bleNumber);
-        // add label to panel
-        result.add(l);
-
-        JLabel bleLabel;
-        BufferedImage ble = ImageIO.read(this.getClass().getResource("/ble.png"));
-        Image bimg = ble.getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        bleLabel = new JLabel(new ImageIcon(bimg));
-        result.add(bleLabel);
-
-
-
-        // create a label to display text
-        l = new JLabel();
-        // add text to label
-        l.setText(""+woodNumber);
-        // add label to panel
-        result.add(l);
-
-
-        JLabel woodLabel;
-        BufferedImage wood = ImageIO.read(this.getClass().getResource("/wood.png"));
-        Image wimg = wood.getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        woodLabel = new JLabel(new ImageIcon(wimg));
-        result.add(woodLabel);
+        result.setLayout(new GridLayout(0, 2));
+        ResourceCard resourceCard;
+        for (Resource r: Resource.values()) {
+            resourceCard = new ResourceCard(r);
+            this.resourceCards.add(resourceCard);
+            this.add(resourceCard);
+        }
         return result;
     }
 
+    public void updateResources() {
+        this.resourceCards.forEach((ResourceCard card) -> {
+            card.revalidate();
+            card.repaint();
+        });
+    }
+
+    /**
+     * Update the current player with a new one
+     * @param currentPlayer The new current player
+     */
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+
+    }
+
+    private class ResourceCard extends JPanel {
+        ResourceCard(Resource resource) throws IOException {
+            JLabel label = new JLabel(""+BottomStatusBar.this.currentPlayer.getResource(resource));
+            JLabel imageLabel = new JLabel(new ImageIcon(resource.getImage()));
+            this.add(label);
+            this.add(imageLabel);
+        }
+    }
 
 }
