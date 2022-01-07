b346871ddd874979c450922b8b340e16f348bd1d
idilsaglam <idilsaglam1@hotmail.com>
2022-01-07 07:32:53 +0100

    refactor(client): BoardPane callback hell refactored

--- client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
+++ client/src/main/java/org/catanuniverse/client/game/board/BoardPane.java
@@ -33,108 +33,21 @@ public class BoardPane extends JPanel {
         this.initSizes(size);
         this.defaultBackground = this.getBackground();
         this.robberLabel = new JLabel("Robber activated");
-        this.robberLabel.setVisible(false);
         this.add(this.robberLabel,BorderLayout.AFTER_LAST_LINE);
-
+        this.desactivateRobber();
         final Dimension sideSize = new Dimension(size.width, size.height / 8),
                 centerSize = new Dimension(size.width, 3 * size.height / 4);
         this.gameSettings = gameSettings;
         this.topStatusPane =
                 new TopStatusBar(this.gameSettings.getPlayers());
         this.gameBoardPane = new GameBoardPane(centerSize);
-        this.boardSidePane = new BoardSidePane((Integer diceValue) -> {
-
-            if(diceValue != null && diceValue== 7){this.activateRobber(); return true;}
-
-            System.out.printf("DICE VALUE %d\n", diceValue);
-            if (this.gameBoardPane.diceRolled(diceValue)) {
-                this.updateStatusBars();
-                return true;
-            }
-            return false;
-        },
-            (Card usedCard) -> {
-                usedCard.use(this.gameSettings.getCurrentPlayer());
-            },
-            (Card stockedCard) -> {
-                stockedCard.stock(this.gameSettings.getCurrentPlayer());
-                this.updateStatusBars();
-            });
-        this.gameBoardPane.setOnRobberMoved((Hextile tile) -> {
-            System.out.printf("Robber moved tile id %d\n", tile.getId());
-            if (this.gameSettings.isRobberActivated() && tile.getPlayable()) {
-                this.gameSettings.setRobberActivated(false);
-                this.desactivateRobber();
-                tile.setPlayable(false);
-                return true;
-            }
-            return false;
-        });
-        this.gameBoardPane.setOnSettlementAdded((Hextile tile, Integer settlementIndex) -> {
-            if (this.gameSettings.isRobberActivated()) return false;
-            if (this.gameSettings.getCurrentPlayer().isAI()) {
-                return false;
-            }
-            try {
-                if (!tile.canAddSettlement(settlementIndex)) return false;
-            } catch (NoSuchSlotException e) {
-                e.printStackTrace();
-                return false;
-            }
-            if (this.gameSettings.getCurrentPlayer().canBuildSettlement()) {
-                System.out.printf("Game board pane on settlement added to %d\n", settlementIndex);
-                if (tile.getSettlementSlot(settlementIndex) != null) {
-                    if (tile.getSettlementSlot(settlementIndex) instanceof City) {
-                        return false;
-                    }
-                    if (tile.getSettlementSlot(settlementIndex).getOwner() == null || !tile.getSettlementSlot(settlementIndex).getOwner().equals(this.gameSettings.getCurrentPlayer())) {
-                        return false;
-                    }
-                    if (!this.gameSettings.getCurrentPlayer().canBuildCity()) return false;
-                    try {
-                        tile.upgradeSettlement(settlementIndex);
-                        this.revalidate();
-                        this.repaint();
-                        this.gameSettings.getCurrentPlayer().buildCity();
-                        return true;
-                    } catch (NoSuchSlotException ignore) {
-                        return false;
-                    }
-                }
-                try {
-                    tile.addSettlement(settlementIndex, new Settlement(this.gameSettings.getCurrentPlayer()));
-                } catch (SlotAlreadyTakenException|NoSuchSlotException ignore) {
-                     return false;
-                }
-                this.revalidate();
-                this.repaint();
-                this.gameSettings.getCurrentPlayer().buildSettlement();
-                return true;
-            }
-            return false;
-        });
-        this.gameBoardPane.setOnRoadAdded((Hextile tile, Integer roadIndex) -> {
-            if (this.gameSettings.isRobberActivated()) return false;
-            if (gameSettings.getCurrentPlayer().isAI()) {
-                // TODO: Make AI play
-                return false;
-            }
-            if (this.gameSettings.getCurrentPlayer().canBuildRoad()) {
-
-            if (tile.getRoadSlot(roadIndex) != null) return false;
-            if (!tile.canAddRoad(roadIndex)) return false;
-            try {
-                tile.addRoad(roadIndex, new Road(this.gameSettings.getCurrentPlayer()));
-                this.revalidate();
-                this.repaint();
-                this.gameSettings.getCurrentPlayer().buildRoad();
-                return true;
-                } catch (SlotAlreadyTakenException | NoSuchSlotException ignore) {
-                    return false;
-                }
-            }
-            return false;
-        });
+        this.boardSidePane = new BoardSidePane(this::onDiceRolled,
+            this::onCardUsed,
+            this::onCardStocked
+            );
+        this.gameBoardPane.setOnRobberMoved(this::onRobberMoved);
+        this.gameBoardPane.setOnSettlementAdded(this::onSettlementAdded);
+        this.gameBoardPane.setOnRoadAdded(this::onRoadAdded);
         this.bottomStatusPane = new BottomStatusBar(
             this.gameSettings.getCurrentPlayer(),
             this.gameSettings.getCurrentPlayerIndex(),
@@ -142,6 +55,134 @@ public class BoardPane extends JPanel {
         );
         this.initPanes(size);
     }
+
+    /**
+     * Verify the given value of the dice
+     * @param diceValue The value of the dice
+     */
+    private boolean onDiceRolled(Integer diceValue) {
+        if(diceValue != null && diceValue== 7){this.activateRobber(); return true;}
+
+        System.out.printf("DICE VALUE %d\n", diceValue);
+        if (this.gameBoardPane.diceRolled(diceValue)) {
+            this.updateStatusBars();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Current player uses the given card card
+     * @param usedCard The card which will be used by the current player
+     */
+    private void onCardUsed(Card usedCard) {
+        usedCard.use(this.gameSettings.getCurrentPlayer());
+    }
+
+    /**
+     * Current player stock a card
+     * @param stockedCard The card to stock
+     */
+    private void onCardStocked(Card stockedCard) {
+        stockedCard.stock(this.gameSettings.getCurrentPlayer());
+        this.updateStatusBars();
+    }
+
+    /**
+     * Verify if the robber can be moved to the given tile
+     * @param tile The tile to move the robber
+     * @return True if the robber can be moved to the given tile, false if not
+     */
+    private boolean onRobberMoved(Hextile tile) {
+        System.out.printf("Robber moved tile id %d\n", tile.getId());
+        if (this.gameSettings.isRobberActivated() && tile.getPlayable()) {
+            this.gameSettings.setRobberActivated(false);
+            this.desactivateRobber();
+            tile.setPlayable(false);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method verify if a settlement can be added to the given tile with given index
+     * @param tile The tile to add the settlement
+     * @param settlementIndex The settlement index to add the settlement
+     * @return True if a settlement can be added to the given slot false if not
+     */
+    private boolean onSettlementAdded(Hextile tile, Integer settlementIndex) {
+        if (this.gameSettings.isRobberActivated()) return false;
+        if (this.gameSettings.getCurrentPlayer().isAI()) {
+            return false;
+        }
+        try {
+            if (!tile.canAddSettlement(settlementIndex)) return false;
+        } catch (NoSuchSlotException e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (this.gameSettings.getCurrentPlayer().canBuildSettlement()) {
+            System.out.printf("Game board pane on settlement added to %d\n", settlementIndex);
+            if (tile.getSettlementSlot(settlementIndex) != null) {
+                if (tile.getSettlementSlot(settlementIndex) instanceof City) {
+                    return false;
+                }
+                if (tile.getSettlementSlot(settlementIndex).getOwner() == null || !tile.getSettlementSlot(settlementIndex).getOwner().equals(this.gameSettings.getCurrentPlayer())) {
+                    return false;
+                }
+                if (!this.gameSettings.getCurrentPlayer().canBuildCity()) return false;
+                try {
+                    tile.upgradeSettlement(settlementIndex);
+                    this.revalidate();
+                    this.repaint();
+                    this.gameSettings.getCurrentPlayer().buildCity();
+                    return true;
+                } catch (NoSuchSlotException ignore) {
+                    return false;
+                }
+            }
+            try {
+                tile.addSettlement(settlementIndex, new Settlement(this.gameSettings.getCurrentPlayer()));
+            } catch (SlotAlreadyTakenException|NoSuchSlotException ignore) {
+                return false;
+            }
+            this.revalidate();
+            this.repaint();
+            this.gameSettings.getCurrentPlayer().buildSettlement();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method handles onRoadAdded event
+     * @param tile The tile on which the road is added
+     * @param roadIndex The index of the road slot to add
+     * @return True if the road can be added
+     */
+    private boolean onRoadAdded(Hextile tile, Integer roadIndex)  {
+            if (this.gameSettings.isRobberActivated()) return false;
+            if (gameSettings.getCurrentPlayer().isAI()) {
+                // TODO: Make AI play
+                return false;
+            }
+            if (this.gameSettings.getCurrentPlayer().canBuildRoad()) {
+
+                if (tile.getRoadSlot(roadIndex) != null) return false;
+                if (!tile.canAddRoad(roadIndex)) return false;
+                try {
+                    tile.addRoad(roadIndex, new Road(this.gameSettings.getCurrentPlayer()));
+                    this.revalidate();
+                    this.repaint();
+                    this.gameSettings.getCurrentPlayer().buildRoad();
+                    return true;
+                } catch (SlotAlreadyTakenException | NoSuchSlotException ignore) {
+                    return false;
+                }
+            }
+            return false;
+    }
+
 
     /** Initialise panes and add them to the current JPanel */
     private void initPanes(Dimension size) {
