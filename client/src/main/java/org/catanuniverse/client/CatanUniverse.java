3fc74d12af93be60d469a285cb0a93426e6476c0
idilsaglam <idilsaglam1@hotmail.com>
2022-01-08 00:32:15 +0100

    music added

--- client/src/main/java/org/catanuniverse/client/CatanUniverse.java
+++ client/src/main/java/org/catanuniverse/client/CatanUniverse.java
@@ -5,9 +5,22 @@
 */
 package org.catanuniverse.client;
 
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class CatanUniverse {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         MainFrame frame = new MainFrame();
+
+        AudioInputStream audioIn = AudioSystem.getAudioInputStream(CatanUniverse.class.getResource("/catanmenu.wav"));
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioIn);
+        clip.start();
     }
 }
