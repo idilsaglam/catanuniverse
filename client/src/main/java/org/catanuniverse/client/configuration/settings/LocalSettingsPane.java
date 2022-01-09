729b84758256691b351f2b43e0d456fc279299ff
idilsaglam <idilsaglam1@hotmail.com>
2022-01-09 03:24:50 +0100

    refactor(client): LocalSettingsPane updated
    Callback functions refactored in separate methods

--- client/src/main/java/org/catanuniverse/client/configuration/settings/LocalSettingsPane.java
+++ client/src/main/java/org/catanuniverse/client/configuration/settings/LocalSettingsPane.java
@@ -18,46 +18,22 @@ final class LocalSettingsPane extends GameSettingsPane<LocalGameSettings> {
 
     protected LocalSettingsPane(Consumer<GameSettings> onGameSettingsChanged) {
         super(onGameSettingsChanged);
-
-        // TODO: Create with default values of sliders
         super.settings =
                 new LocalGameSettings(
                         GameSettingsPane.DEFAULT_CAPACITY, GameSettingsPane.DEFAULT_NB_AI);
 
         this.difficultySelector =
-                new DifficultySelector(
-                        (Difficulty difficulty) -> {
-                            System.out.printf("Difficulty level changed to %s\n", difficulty);
-                            super.settings.setDifficulty(difficulty);
-                            super.onGameSettingsChanged.accept(super.settings);
-                        });
+                new DifficultySelector(this::onDifficultySelectorUpdated);
 
         this.numberOfAISelector =
                 new NumberOfAISelector(
                         GameSettingsPane.DEFAULT_NB_AI,
-                        (Integer numberOfAI) -> {
-                            System.out.printf("Number of AI slider value %d\n", numberOfAI);
-                            boolean disable = numberOfAI == 0;
-                            if (this.difficultySelector.isSliderEnabled() == disable) {
-                                this.difficultySelector.setValue(0);
-                                System.out.printf("Difficulty slider will be enabled %b", !disable);
-                                this.difficultySelector.setSliderEnabled(!disable);
-                                this.difficultySelector.repaintSlider();
-                            }
-                            super.settings.setNumberOfAI(numberOfAI);
-                            super.onGameSettingsChanged.accept(super.settings);
-                        });
+                        this::onAISelectorUpdated);
 
         this.capacitySelector =
                 new CapacitySelector(
                         GameSettingsPane.DEFAULT_CAPACITY,
-                        (Integer capacity) -> {
-                            System.out.printf("Capacity slider value %d\n", capacity);
-                            this.numberOfAISelector.setMaximum(capacity - 1);
-                            this.numberOfAISelector.repaintSlider();
-                            super.settings.setCapacity(capacity);
-                            super.onGameSettingsChanged.accept(super.settings);
-                        });
+                        this::onCapacitySelectorUpdated);
         this.add(this.capacitySelector);
         this.add(this.numberOfAISelector);
         this.add(this.difficultySelector);
@@ -67,4 +43,44 @@ final class LocalSettingsPane extends GameSettingsPane<LocalGameSettings> {
     boolean isSettingsValid() {
         return true;
     }
+
+  /**
+   * Handles the update of the difficulty selector
+   * @param difficulty The difficulty value in the difficulty selector
+   */
+  private void onDifficultySelectorUpdated(Difficulty difficulty) {
+    System.out.printf("Difficulty level changed to %s\n", difficulty);
+    super.settings.setDifficulty(difficulty);
+    super.onGameSettingsChanged.accept(super.settings);
+  }
+
+  /**
+   * Handles the capacity selector update event
+   * @param capacity The value of the capacity selector
+   */
+  private void onCapacitySelectorUpdated(Integer capacity) {
+    System.out.printf("Capacity slider value %d\n", capacity);
+    this.numberOfAISelector.setMaximum(capacity - 1);
+    this.numberOfAISelector.repaintSlider();
+    super.settings.setCapacity(capacity);
+    super.onGameSettingsChanged.accept(super.settings);
+  }
+
+  /**
+   * Handles the AI selector updated event
+   * @param numberOfAI The number of AIs selected in AI selector
+   */
+  private void onAISelectorUpdated(Integer numberOfAI) {
+    System.out.printf("Number of AI slider value %d\n", numberOfAI);
+    boolean disable = numberOfAI == 0;
+    if (this.difficultySelector.isSliderEnabled() == disable) {
+      this.difficultySelector.setValue(0);
+      System.out.printf("Difficulty slider will be enabled %b", !disable);
+      this.difficultySelector.setSliderEnabled(!disable);
+      this.difficultySelector.repaintSlider();
+    }
+    super.settings.setNumberOfAI(numberOfAI);
+    super.onGameSettingsChanged.accept(super.settings);
+
+  }
 }
